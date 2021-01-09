d7546ab46ed0d27b208ad118558603d17a44eee9
Piotr Loba <piotr.loba22@gmail.com>
2021-01-09 10:23:56 +0100

    for 9.4 - added detailed view and navi bar for list type

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
-# Readme for Kodilla task 10.3 - Start Bootstrap
+# Readme for Kodilla task 10.4 - Start Bootstrap
 
 Main page might be on below address.
 > <http://127.0.0.1:5000/>
 
-8 elementów bez kombinowania.
-Wycenrowałem "Tytuł filmu".
-
-Wyświetlane są randomowe filmy.
+* Just 8 movies on the view;
+* Centered movie title
+* The movie details view has a random image from the movie.
+* Cast - as much as the factory has given.
+* Popular - random videos are shown. The rest follows API order.
--- tmdb_client.py
+++ tmdb_client.py
@@ -1,22 +1,34 @@
 import requests
 import random
 
+API_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhZTQxNDVhYWZmYTQ4MDQ5ODRkYTRlNDU2ODY1Y2IxNyIsInN1YiI6IjVmZjdiMWI4OTFmMGVhMDAzZjkyOTc2NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.y8xBRnexLAYN_tyO7lnXMHExTcoAs2LcJXj4XKcxIj8"
+
+
 def get_popular_movies():
     endpoint = "https://api.themoviedb.org/3/movie/popular"
-    api_token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhZTQxNDVhYWZmYTQ4MDQ5ODRkYTRlNDU2ODY1Y2IxNyIsInN1YiI6IjVmZjdiMWI4OTFmMGVhMDAzZjkyOTc2NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.y8xBRnexLAYN_tyO7lnXMHExTcoAs2LcJXj4XKcxIj8"
     headers = {
-        "Authorization": f"Bearer {api_token}"
+        "Authorization": f"Bearer {API_TOKEN}"
     }
     response = requests.get(endpoint, headers=headers)
     return response.json()
 
 
-def generate_movies():
+def get_movies_list(list_type="popular"):
+    endpoint = f"https://api.themoviedb.org/3/movie/{list_type}"
+    headers = {
+        "Authorization": f"Bearer {API_TOKEN}"
+    }
+    response = requests.get(endpoint, headers=headers)
+    response.raise_for_status()
+    return response.json()
+
+
+def generate_random_movies(list_type):
     """
     Randomise Popular Movies
     """
     data = []
-    movies = get_popular_movies()["results"]
+    movies = get_movies_list(list_type)["results"]
 
     for _ in range(len(movies)):
         show = random.choice(movies)
@@ -25,20 +37,47 @@ def generate_movies():
     return data
 
 
-def get_movies(how_many):
-    data = generate_movies()
+def get_movies(how_many, list_type):
+    """
+    Generate random movies from popular type.
+    For others types there is no randomise.
+    """
+    if list_type == "popular":
+        data = generate_random_movies(list_type)
+    else:
+        data = get_movies_list(list_type)["results"]
     return data[:how_many]
-
-
-"""
-def get_movies(how_many):
-    data = get_popular_movies()
-    return data["results"][:how_many]
-"""
 
 
 def get_poster_url(poster_api_path, size="w342"):
     base_url = "https://image.tmdb.org/t/p/"
     return f"{base_url}{size}/{poster_api_path}"
+
+
+def get_single_movie(movie_id):
+    endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}"
+    headers = {
+        "Authorization": f"Bearer {API_TOKEN}"
+    }
+    response = requests.get(endpoint, headers=headers)
+    return response.json()
+
+
+def get_single_movie_cast(movie_id):
+    endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}/credits"
+    headers = {
+        "Authorization": f"Bearer {API_TOKEN}"
+    }
+    response = requests.get(endpoint, headers=headers)
+    return response.json()["cast"]
+
+
+def get_movie_images(movie_id):
+    endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}/images"
+    headers = {
+        "Authorization": f"Bearer {API_TOKEN}"
+    }
+    response = requests.get(endpoint, headers=headers)
+    return response.json()
 
 
